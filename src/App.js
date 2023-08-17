a561e59e42275a1735a1ebb84815052292853120
roshani2022 <rosh.gupta17@gmail.com>
2023-08-17 20:23:48 +0530

    Consumer context Api

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContet from "./Store/auth-context";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -28,13 +29,19 @@ function App() {
   };
 
   return (
-    <React.Fragment>
+  
+      <AuthContet.Provider value={{
+        isLoggedIn:isLoggedIn
+      }
+        
+      }>
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+      </AuthContet.Provider>
+    
   );
 }
 
---
+++ src/Store/auth-context.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AuthContet = React.createContext({
+  isLoggedIn:false
+})
+
+export default AuthContet;
